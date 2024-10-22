c3dd33fa935029b58cefff1918b4fba1e800a12e
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-10-22 13:17:03 +0500

    Adil Khosa

---
+++ app.js
@@ -0,0 +1,91 @@
+// var input = document.getElementById("inputField");
+
+
+// function getValue(number) {
+//     input.value += number;
+// }
+
+// function clearAll() {
+//     input.value = "";
+// }
+
+// function deleteChar() {
+//     input.value = input.value.slice(0,-1);
+// }
+
+// function square() {
+//     input.value = input.value * input.value;
+// }
+
+// function equal() {
+//     input.value = eval(input.value);
+// }
+
+
+
+
+// var div = document.getElementById('main');
+
+// console.log(div);
+
+// var paras = div.getElementsByTagName('p');
+
+// // console.log(paras[0]);
+// // console.log(paras[1]);
+// // console.log(paras[2]);
+// // console.log(paras[3]);
+
+// for ( var i = 0 ; i < paras.length ; i++ ) {
+
+//     paras[i].style.color = 'blue';
+//     paras[i].style.textDecoration = 'underline';
+//     paras[i].style.fontStyle = 'italic';
+//     paras[i].style.border = '2px solid black';
+
+//     console.log(paras[i]);
+
+// }
+
+
+
+// var div = document.getElementById('box');
+
+// console.log(div);
+
+// var anchor = div.getElementsByTagName('a');
+
+// for ( var i = 0 ; i < anchor.length ; i++ ) {
+
+//     console.log(anchor[i]);
+
+// }
+
+
+
+
+
+// setInterval()
+
+
+// var interval;
+
+// function stop() {
+//     clearInterval(interval);
+// }
+
+// function timer() {
+//     console.log("function call...");
+// }
+
+// interval = setInterval(timer,1000); 
+
+
+
+// setTimeout()
+
+
+function timer() {
+    alert("Welcome User.")
+}
+
+setTimeout(timer,3000);
